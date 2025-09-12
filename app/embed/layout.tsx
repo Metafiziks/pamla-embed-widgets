798d35e5f8e61484eee3d1e78222616b7829587c
Metafiziks <info@original-legacy.com>
2025-09-12 14:40:33 +0800

    chore: add missing layout file

---
+++ app/embed/layout.tsx
@@ -0,0 +1,8 @@
+// app/embed/layout.tsx
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+export const fetchCache = 'force-no-store'
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>
+}
