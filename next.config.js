ab6c954dab367247e0653cf23847d11fc0aa1cd7
Metafiziks <info@original-legacy.com>
2025-10-22 06:49:29 +0800

    Update build commands

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
-}
-module.exports = nextConfig
+};
+
+export default nextConfig;
